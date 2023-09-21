69a1da5570adb3d7737b5a414d435733eed96b56
SatviK1608 <satvik1608@gmail.com>
2023-09-21 10:59:58 +0530

    stacks ques

---
+++ stacks/ImplementTwoStacksUsingArray.java
@@ -0,0 +1,14 @@
+package stacks;
+
+public class ImplementTwoStacksUsingArray {
+
+	public ImplementTwoStacksUsingArray() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
