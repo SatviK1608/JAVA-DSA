fe5c01915294fcf142be3cc2b2db98c7e2b5bb29
SatviK1608 <satvik1608@gmail.com>
2023-12-11 10:52:02 +0530

    Delete Node

--- trees/binaryTree/binarySearchTree/BinarySearchTree.java
+++ trees/binaryTree/binarySearchTree/BinarySearchTree.java
@@ -4,9 +4,9 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 class Node {
-    private int value;
-    private Node left;
-    private Node right;
+    public int value;
+    public Node left;
+    public Node right;
 
     public Node(int value) {
         this.value = value;
@@ -45,7 +45,9 @@ public class BinarySearchTree {
     public BinarySearchTree() {
         root = null;
     }
-
+    public Node getRoot(){
+    	return root;
+    }
     public void add(int value) {
         root = addNode(root, value);
     }
@@ -64,6 +66,47 @@ public class BinarySearchTree {
 
         return currentRoot;
     }
+    
+    private int findMinNode(Node subRoot){
+    	int min=subRoot.value;
+    	
+    	while(subRoot.left!=null){
+    		min=subRoot.left.value;
+    		subRoot=subRoot.left;
+    	}
+    	return min;
+    }
+    
+    private Node deleteNode(Node root,int value){
+    	if(root==null)
+    		return root;
+    	if(value<root.getValue())
+    		root.left=deleteNode(root.left,value);
+    	else if(value>root.value)
+    		root.right=deleteNode(root.right,value);
+    	else
+    		if(root.left==null)
+    			return root.right;
+    		else if(root.right==null)
+    			return root.left;
+    		else
+    			root.value=findMinNode(root.right);
+    			root.right=deleteNode(root.right,root.value);
+    			
+    	return root;	
+    	
+    }
+    
+    
+    
+    /*
+     * To delete a node their are two approaches 
+     * 	Approach 1-> i. Choose the largest ele from left sub tree
+     * 				ii. 
+     * 	Approach 2-> i. Choose the smallest ele from right sub tree
+     * 				ii. Replace the value of node to be deleted with the chosen smallest ele
+     * 			   iii. Remove the min ele from right sub tree
+     */
 
     public void levelOrderTraversal() {
         levelOrderTraversal(root);
@@ -109,18 +152,22 @@ public class BinarySearchTree {
         bst.add(25);
         bst.add(5);
         bst.add(7);
+        bst.add(6);
         bst.add(12);
         bst.add(-4);
         bst.add(18);
-        bst.add(6);
+        bst.add(8);
 
         /*
                     10
               1            15
           -4     5     12     20
                    7        18    25
-                 6
+                 	8
          */
+        //bst.deleteNode(bst.getRoot(),7);
+        //bst.deleteNode(bst.getRoot(),1);
+        
 
         bst.levelOrderTraversal();
 
